00450ab4b8a9b4ae5ea39dc6a6b74bf119e820b0
Cianan <B00164511@mytudublin.ie>
2025-12-06 23:44:11 +0000

    connected to firebase

---
+++ DeoApp/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRi2WrzJiuhe7ycsRNZcwmglqcLTPIfEc",
+  authDomain: "deoapp-56eb9.firebaseapp.com",
+  projectId: "deoapp-56eb9",
+  storageBucket: "deoapp-56eb9.firebasestorage.app",
+  messagingSenderId: "857633752168",
+  appId: "1:857633752168:web:556f666e1d657827ad27dd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
